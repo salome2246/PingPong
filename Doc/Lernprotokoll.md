cf4b0e59a7df6058f3b7b64fbc3a9fc95ead9c73
sgi059130 <sgi059130@stud.gibb.ch>
2025-12-17 15:59:40 +0100

    versuch fehlerbehebung zu implementieren

---
+++ Doc/Lernprotokoll.md
@@ -0,0 +1,23 @@
+
+
+
+# Lernprotokoll
+
+
+## Versionisieren
+
+GIT TAGS:
+Ich habe herausgefunden, dass ich meine Commits mit Tags versehen kann.
+Die einzelnen Teilaufgaben sind so im Repository leicht zu finden.
+
+
+## Syntax Python
+
+
+
+## Client-Server-Kommunikation
+## UDP
+## Sockets
+## Ports
+
+
--- client.py
+++ client.py
@@ -1,9 +1,13 @@
 import socket
+import time
+import random
 
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 5005
+TIMEOUT = 2
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(TIMEOUT)
 
 print("wähle eine Zahl zwischen 1 und 12")
 
@@ -11,18 +15,24 @@ spin = int(input("Gib eine Zahl ein: "))
 print(f"Startwert: {spin}")
 
 while spin < 12:
-    # Ping: aktuelle Zahl senden
-    sock.sendto(str(spin).encode(), (SERVER_IP, SERVER_PORT))
-    print(f"Ping gesendet: {spin}")
+    try:
+        # Ping: aktuelle Zahl senden
+        sock.sendto(str(spin).encode(), (SERVER_IP, SERVER_PORT))
+        print(f"Ping gesendet: {spin}")
+        # Pong: Antwort empfangen (spin + 1)
+        data, addr = sock.recvfrom(1024)
+        spin = int(data.decode())
+        print(f"Pong empfangen: {spin}")
+        time.sleep(3)
 
-    # Pong: Antwort empfangen (spin + 1)
-    data, addr = sock.recvfrom(1024)
-    spin = int(data.decode())
-    print(f"Pong empfangen: {spin}")
-
-    # Vorbereitung für nächsten Ping (spin + 2 insgesamt)
-    spin += 1
+        # Vorbereitung für nächsten Ping (spin + 2 insgesamt)
+        spin += 1
+    except socket.timeout:
+        print("Fehler: Timeout")
+    except Exception as e:
+        print("fehler:" + str(e))
 
 print("12 erreicht oder überschritten – Ende.")
 sock.close()
+
 
