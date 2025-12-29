35056c4c4d43746adb4d3bd521b9ddc255f3226a
sgi059130 <sgi059130@stud.gibb.ch>
2025-12-29 12:25:19 +0100

    server vereinfacht für bessere Übersicht.

--- server.py
+++ server.py
@@ -1,25 +1,21 @@
 import socket
-import random
-import time
 
-SERVER_IP = "0.0.0.0"
+SERVER_IP = "127.0.0.1"
 SERVER_PORT = 6000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((SERVER_IP, SERVER_PORT))
 
-print("UDP Server läuft...")
+print("Server läuft...")
 
-while True:
-    data, addr = sock.recvfrom(1024)
-    spin = int(data.decode())
+data, addr = sock.recvfrom(1024)
+spin = int(data.decode())
+print(f"Server empfängt Ping: {spin}")
 
-    print(f"Pong erhalten: {spin}")
-    # Verzögerung zufällig zwischen 1 und 5 Sekunden
-   # delay = random.randint(1, 5)
-    #time.sleep(delay)
+# Server verändert den Spin
+spin += 1
+sock.sendto(str(spin).encode(), addr)
 
-    spin += 1
-    sock.sendto(str(spin).encode(), addr)
+print(f"Server sendet Pong: {spin}")
 
-    print(f"Pong gesendet: {spin}")
+sock.close()
